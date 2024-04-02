2076c5ba08563af017cb6dcaacfdb05fa7051ed6
Doha Hamdali <152738024+Doha-H@users.noreply.github.com>
2024-04-02 02:09:53 +0200

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,18 @@
+# Il s'agit d'un programme permettant de générer un mot de passe aléatoire.
+# Ce programme offre la possibilité à l'utilisateur de choisir les conditions nécessaires pour rendre son mot de passe robuste. 
+# (longueur minimale, chiffres, caractères spéciaux).
+
+# Ici, je suis amenée à mettre en pratique l'importation de modules, une boucle while, des conditionnelles if/elif. 
+# Je définis ma propre fonction generate_passwod() et j'utilise une fonction input() pour recevoir les entrées de l'utilisateur.
+
+
 import random
-import string
+import string  # Importation du module string pour utiliser des chaînes de caractères prédéfinies
 
 def generate_password(min_Length, numbers=True, special_characters=True):
-    letters = string.ascii_letters
-    digits = string.digits
-    special = string.punctuation
+    letters = string.ascii_letters  # cette constante représente l'ensemble des lettres de l'alphabet, minuscules et majuscules
+    digits = string.digits          # celle-ci repésente les chiffres de 0 à 9
+    special = string.punctuation    # celle-ci repésente les caractères de ponctuation
 
     characters = letters
     if numbers:
@@ -18,8 +26,7 @@ def generate_password(min_Length, numbers=True, special_characters=True):
     has_number = False
     has_special = False
 
-    while not meets_criteria or len(pwd) < min_Length:
-        new_char = random.choice(characters)
+    while not meets_criteria or len(pwd) < min_Length:    # Boucle pour générer le mot de passe jusqu'à ce qu'il réponde aux conditions spécifiées
         pwd += new_char
 
         if new_char in digits:
@@ -41,6 +48,3 @@ has_number = input("Do you want to have numbers (y/n)?").lower() == "y"
 has_special = input("Do you want to have special characters (y/n)?").lower() == "y"
 pwd = generate_password(min_length, has_number, has_special)
 print("The generated password is", pwd)
-
-
-
